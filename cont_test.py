1994d1b686f1c645030224c07179937919880b0b
MarkMk1 <markbradyeire@gmail.com>
2018-11-02 13:26:22 +0100

    Test for controller

---
+++ cont_test.py
@@ -0,0 +1,21 @@
+# !/usr/bin/python3
+import communication as comm
+import RPi.GPIO as io
+
+io.setmode(io.BCM)
+io.setup(5,io.OUT)
+io.setup(6,io.OUT)
+io.setup(12,io.OUT)
+io.setup(13,io.OUT)
+io.setup(9,io.OUT)
+io.setup(10,io.OUT)
+buttons = [5,6,12,13,9,10]
+
+while True:
+    for i in range(0,5)
+        x = comm.queryController(i)
+        if x:
+            io.output(buttons[i], 1)
+        else: 
+            io.output(buttons[i], 0)
+
